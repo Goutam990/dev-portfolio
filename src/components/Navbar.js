c201665c4ee7922734be80a90a2b6c3fc83e525a
Goutam Tiwari <102357588+Goutam990@users.noreply.github.com>
2025-07-28 14:15:06 +0530

    Github button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -104,7 +104,7 @@ function NavBar() {
 
             <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/Goutam990/dev-portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
